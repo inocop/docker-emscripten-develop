46587d7a0c78af420257bab48f0dd561eabe284a
inocop <inoue19851110@gmail.com>
2018-03-02 18:37:31 +0900

    fix build shell

--- src/build_asm.sh
+++ src/build_asm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OUTPUT_DIR=/opt/novnc/em_asm
+OUTPUT_DIR=`dirname $1`
+mkdir -p ${OUTPUT_DIR}/build_asm
 
-mkdir -p ${OUTPUT_DIR}
-emcc $1 -s LEGACY_GL_EMULATION=1 -o ${OUTPUT_DIR}/index.html
+emcc $1 -s LEGACY_GL_EMULATION=1 -o ${OUTPUT_DIR}/build_asm/asm.html
--- src/build_wasm.sh
+++ src/build_wasm.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
+OUTPUT_DIR=`dirname $1`
+mkdir -p ${OUTPUT_DIR}/build_wasm
 
-OUTPUT_DIR=/opt/novnc/em_wasm
-
-mkdir -p ${OUTPUT_DIR}
-emcc $1 -s WASM=1 -s LEGACY_GL_EMULATION=1 -o ${OUTPUT_DIR}/index.html
+emcc $1 -s WASM=1 -s LEGACY_GL_EMULATION=1 -o ${OUTPUT_DIR}/build_wasm/wasm.html
