f5799bd8ac8358aeee74a512ebf0cf60a80f10b6
inocop <inoue19851110@gmail.com>
2018-03-26 21:44:45 +0900

    fix output dir

--- src/build_asm.sh
+++ src/build_asm.sh
@@ -3,4 +3,4 @@
 OUTPUT_DIR=`dirname $1`
 mkdir -p ${OUTPUT_DIR}/build_asm
 
-emcc $1 -s LEGACY_GL_EMULATION=1 -s LEGACY_VM_SUPPORT=1 -o ${OUTPUT_DIR}/build_asm/asm.html
+emcc $1 -s LEGACY_GL_EMULATION=1 -s LEGACY_VM_SUPPORT=1 -o ${OUTPUT_DIR}/output_asm/asm.html
--- src/build_wasm.sh
+++ src/build_wasm.sh
@@ -3,4 +3,4 @@
 OUTPUT_DIR=`dirname $1`
 mkdir -p ${OUTPUT_DIR}/build_wasm
 
-emcc $1 -s WASM=1 -s LEGACY_GL_EMULATION=1 -s LEGACY_VM_SUPPORT=1 -o ${OUTPUT_DIR}/build_wasm/wasm.html
+emcc $1 -s WASM=1 -s LEGACY_GL_EMULATION=1 -s LEGACY_VM_SUPPORT=1 -o ${OUTPUT_DIR}/output_wasm/wasm.html
